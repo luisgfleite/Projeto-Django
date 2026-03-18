82969ce15693001c58deb6e3cdcc13fab88ff35d
Luis Gustavo <luis.devv@gmail.com>
2026-03-18 15:48:34 -0300

    importando no db

--- appone/models.py
+++ appone/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
